891e6c40595857b4e023150113a972240c95d01c
ErikMoczi <32465761+ErikMoczi@users.noreply.github.com>
2025-03-10 23:33:09 +0100

    Modelu 4

---
+++ BethanysPieShopHRM/Components/ProfilePicture.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components;
+
+public partial class ProfilePicture
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
--- BethanysPieShopHRM/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM/Pages/EmployeeOverview.razor.cs
@@ -8,8 +8,17 @@ public partial class EmployeeOverview : ComponentBase
 {
     public List<Employee>? Employees { get; set; } = default!;
 
+    private Employee? _selectedEmployee;
+
+    private string Title = "Employee Overview";
+
     protected override void OnInitialized()
     {
         Employees = MockDataService.Employees;
+    }
+
+    private void ShowQuickViewPopup(Employee selectedEmployee)
+    {
+        _selectedEmployee = selectedEmployee;
     }
 }
